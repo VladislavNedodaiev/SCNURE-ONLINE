a924f6e3924a197e9769bbc9635c36ee26a27f19
Mykhailo Plotnikov <Mykhailo.Plotnikov@cruxlab.com>
2019-10-25 00:10:40 +0300

    Add /api/startups/startup endpoint

---
+++ SCNURE-BACKEND/SCNURE-BACKEND/Controllers/StartupsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SCNURE_BACKEND.Data.Entities.ClientEntities.Startup;
+using SCNURE_BACKEND.Services.Users;
+
+namespace SCNURE_BACKEND.Controllers
+{
+    [Route("api/startups")]
+    [ApiController]
+    public class StartupsController : ControllerBase
+    {
+        private readonly RemoteStartupMapper remoteStartupMapper = new RemoteStartupMapper(); 
+        private readonly IStartupService startupService;
+        public StartupsController(IStartupService _startupService)
+        {
+            startupService = _startupService;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("startup")]
+        public async Task<IActionResult> GetStartupAsync([Required]int id)
+        {
+            try
+            {
+                var remoteStartup = await startupService.GetStartupById(id);
+                var clientResult = remoteStartupMapper.MapRemoteStartup(remoteStartup);
+                return Ok(clientResult);
+            }
+            catch(Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+    }
+}
---
+++ SCNURE-BACKEND/SCNURE-BACKEND/Services/Startups/StartupService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Options;
+using SCNURE_BACKEND.Data;
+using SCNURE_BACKEND.Helpers;
+using System.Threading.Tasks;
+
+namespace SCNURE_BACKEND.Services.Users
+{
+    public interface IStartupService
+    {
+        Task<Data.Entities.Startup> GetStartupById(int startupId);
+    }
+
+    public class StartupServiceImpl : IStartupService
+    {
+        private readonly SCContext _dbcontext;
+        private readonly JwtSettings _jwtSettings; //Tokens
+
+        public StartupServiceImpl(SCContext sCContext, IOptions<JwtSettings> jwtSettings)
+        {
+            _dbcontext = sCContext;
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        public Task<Data.Entities.Startup> GetStartupById(int startupId)
+        {
+            return _dbcontext.Startups.FindAsync(startupId);
+        }
+    }
+}
