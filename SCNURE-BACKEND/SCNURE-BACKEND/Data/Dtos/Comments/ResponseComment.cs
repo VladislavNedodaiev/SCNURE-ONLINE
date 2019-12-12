d18e66433f3e50c60fd2d73f02c42b606b8b47ea
Mykhailo Plotnikov <mykhailo.plotnikov@cruxlab.com>
2019-12-12 22:09:07 +0200

    Fix crashes

---
+++ SCNURE-BACKEND/SCNURE-BACKEND/Data/Dtos/Comments/ResponseComment.cs
@@ -0,0 +1,30 @@
+﻿using SCNURE_BACKEND.Data.Dtos.Mappers;
+using SCNURE_BACKEND.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SCNURE_BACKEND.Data.Dtos.Comments
+{
+    public class ResponseComment
+    {
+        public ResponseComment() { }
+
+        public ResponseComment(User user, Comment comment)
+        {
+            CommentId = comment.CommentId;
+            StartupId = comment.StartupId;
+            UserInfo = UserMapper.ToUserInfoResponse(user);
+            Text = comment.Text;
+        }
+
+        public int CommentId { get; set; }
+
+        public int StartupId { get; set; }
+
+        public UserInfo  UserInfo { get; set; }
+
+        public string Text { get; set; }
+    }
+}
